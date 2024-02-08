a0823b9f744608bdee08ac92fb75a81f288da1cf
Brian Gluzman <bgluzman@gmail.com>
2024-02-08 15:02:06 -0500

    make System private inside SystemRegistry

--- src/SystemRegistry.cpp
+++ src/SystemRegistry.cpp
@@ -1,3 +1,13 @@
 #include "SystemRegistry.h"
 
-namespace bad {}  // namespace bad
+namespace bad {
+
+void SystemRegistry::run(ComponentRegistry& components) {
+  for (System& system : systems_) {
+    for (EntityId id : system.getQueryComponents(components)) {
+      system.execute(components, id);
+    }
+  }
+}
+
+}  // namespace bad
--- src/SystemRegistry.h
+++ src/SystemRegistry.h
@@ -10,61 +10,6 @@
 
 namespace bad {
 
-// TODO (bgluzman): support first arg being EntityHandle
-class System {
-public:
-  template <Component... Args>
-  static System construct(std::invocable<Args...> auto&& system) {
-    return System(std::forward<decltype(system)>(system), tag<Args...>{});
-  }
-
-  std::vector<EntityId> getQueryComponents(ComponentRegistry& components) {
-    return system_->getQueryComponents(components);
-  }
-  void execute(ComponentRegistry& components, EntityId entityId) {
-    system_->execute(components, entityId);
-  }
-
-private:
-  // TODO (bgluzman): can this be done with a deduction template instead?
-  template <Component... Args>
-  struct tag {};
-  template <Component... Args>
-  explicit System(std::invocable<Args...> auto&& system, tag<Args...>)
-      : system_(std::make_unique<SystemImpl<Args...>>(
-            std::forward<decltype(system)>(system))) {}
-
-  struct SystemInterface {
-    virtual ~SystemInterface() noexcept = default;
-    virtual std::vector<EntityId>
-                 getQueryComponents(ComponentRegistry& components) = 0;
-    virtual void execute(ComponentRegistry& components,
-                         EntityId           entityId) const = 0;
-  };
-
-  template <Component... Args>
-  struct SystemImpl : public SystemInterface {
-  public:
-    explicit SystemImpl(std::invocable<Args...> auto&& system)
-        : system_(std::forward<decltype(system)>(system)) {}
-
-    std::vector<EntityId>
-    getQueryComponents(ComponentRegistry& components) override {
-      return components.getQueryComponents<Args...>();
-    }
-
-    void execute(ComponentRegistry& components,
-                 EntityId           entityId) const override {
-      system_(components.getUnchecked<Args>(entityId)...);
-    }
-
-  private:
-    std::function<void(Args...)> system_;
-  };
-
-  std::unique_ptr<SystemInterface> system_;
-};
-
 class SystemRegistry {
 public:
   template <Component... Args>
@@ -72,6 +17,61 @@ public:
   void run(ComponentRegistry& components);
 
 private:
+  // TODO (bgluzman): support first arg being EntityHandle
+  class System {
+  public:
+    template <Component... Args>
+    static System construct(std::invocable<Args...> auto&& system) {
+      return System(std::forward<decltype(system)>(system), tag<Args...>{});
+    }
+
+    std::vector<EntityId> getQueryComponents(ComponentRegistry& components) {
+      return system_->getQueryComponents(components);
+    }
+    void execute(ComponentRegistry& components, EntityId entityId) {
+      system_->execute(components, entityId);
+    }
+
+  private:
+    // TODO (bgluzman): can this be done with a deduction template instead?
+    template <Component... Args>
+    struct tag {};
+    template <Component... Args>
+    explicit System(std::invocable<Args...> auto&& system, tag<Args...>)
+        : system_(std::make_unique<SystemImpl<Args...>>(
+              std::forward<decltype(system)>(system))) {}
+
+    struct SystemInterface {
+      virtual ~SystemInterface() noexcept = default;
+      virtual std::vector<EntityId>
+                   getQueryComponents(ComponentRegistry& components) = 0;
+      virtual void execute(ComponentRegistry& components,
+                           EntityId           entityId) const = 0;
+    };
+
+    template <Component... Args>
+    struct SystemImpl : public SystemInterface {
+    public:
+      explicit SystemImpl(std::invocable<Args...> auto&& system)
+          : system_(std::forward<decltype(system)>(system)) {}
+
+      std::vector<EntityId>
+      getQueryComponents(ComponentRegistry& components) override {
+        return components.getQueryComponents<Args...>();
+      }
+
+      void execute(ComponentRegistry& components,
+                   EntityId           entityId) const override {
+        system_(components.getUnchecked<Args>(entityId)...);
+      }
+
+    private:
+      std::function<void(Args...)> system_;
+    };
+
+    std::unique_ptr<SystemInterface> system_;
+  };
+
   std::vector<System> systems_ = {};
 };
 
@@ -79,14 +79,6 @@ template <Component... Args>
 void SystemRegistry::add(std::invocable<Args...> auto&& system) {
   systems_.emplace_back(
       System::construct<Args...>(std::forward<decltype(system)>(system)));
-}
-
-inline void SystemRegistry::run(ComponentRegistry& components) {
-  for (System& system : systems_) {
-    for (EntityId id : system.getQueryComponents(components)) {
-      system.execute(components, id);
-    }
-  }
 }
 
 }  // namespace bad
