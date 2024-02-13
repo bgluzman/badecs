ea32227e16fc2d2244407fb8a7debd555ea98917
Brian Gluzman <bgluzman@gmail.com>
2024-02-12 20:52:36 -0500

    remove indirection from Column

--- src/Column.h
+++ src/Column.h
@@ -18,28 +18,37 @@ public:
   template <Component T, typename... Ts>
   void emplace(EntityId entityId, Ts&&...args);
   template <Component T>
-  gsl::not_null<T *> get(EntityId entityId);
+  T *get(EntityId entityId);
   template <Component T>
   void set(EntityId entityId, const T& value);
 
 private:
-  std::unordered_map<EntityId, std::unique_ptr<std::any>> components_ = {};
+  // XXX: std::unordered_map's pointer/reference invalidation semantics here are
+  //  incredibly important since get() returns a pointer into the map. If
+  //  another map implementation is chosen, get() should have different
+  //  semantics OR the values in the map should be pointers. See:
+  //  https://en.cppreference.com/w/cpp/container/unordered_map#Iterator_invalidation
+  std::unordered_map<EntityId, std::any> components_ = {};
 };
 
 template <Component T, typename... Ts>
 void Column::emplace(EntityId entityId, Ts&&...args) {
-  components_[entityId] = std::make_unique<std::any>(std::in_place_type<T>,
-                                                     std::forward<Ts>(args)...);
+  components_[entityId] =
+      std::any(std::in_place_type<T>, std::forward<Ts>(args)...);
 }
 
 template <Component T>
-gsl::not_null<T *> Column::get(EntityId entityId) {
-  return std::any_cast<T>(components_[entityId].get());
+T *Column::get(EntityId entityId) {
+  if (auto it = components_.find(entityId); it != components_.end()) {
+    return &std::any_cast<T&>(it->second);
+  } else {
+    return nullptr;
+  }
 }
 
 template <Component T>
 void Column::set(EntityId entityId, const T& value) {
-  components_[entityId] = std::make_unique<std::any>(value);
+  components_[entityId] = value;
 }
 
 }  // namespace bad
