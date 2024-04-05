43ddc3a2263add4733ee8448d54ed8b2413afe16
Brian Gluzman <bgluzman@gmail.com>
2024-04-05 14:44:08 -0700

    empty invariants test

--- test/badecs_test.cpp
+++ test/badecs_test.cpp
@@ -4,10 +4,16 @@
 
 namespace bad::internal {
 
-TEST(ColumnTest, EmplaceTest) {
+TEST(ColumnTest, EmptyInvariants) {
+  Column column;
+  EXPECT_EQ(column.size(), 0);
+  EXPECT_EQ(column.has(0), false);
+}
+
+TEST(ColumnTest, Emplace) {
   Column column;
   ASSERT_EQ(column.size(), 0);
-  EXPECT_EQ(column.has(0), false);
+  ASSERT_EQ(column.has(0), false);
   column.emplace<int>(0, 1);
   EXPECT_EQ(column.has(0), true);
   EXPECT_EQ(column.size(), 1);
