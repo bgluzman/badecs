49434497e2fe677c4bfe981e7389bc187784549b
Brian Gluzman <bgluzman@gmail.com>
2024-04-09 15:38:24 -0700

    redo skeleton using templates

--- include/badecs/internal/Components.h
+++ include/badecs/internal/Components.h
@@ -1,39 +1,51 @@
 #ifndef BADECS_INTERNAL_COMPONENTS_H
 #define BADECS_INTERNAL_COMPONENTS_H
 
+#include <any>
 #include <badecs/Common.h>
 #include <badecs/internal/Column.h>
-
-#include <any>
+#include <ranges>
 
 namespace bad::internal {
 
-// TODO (bgluzman): docstring
+/// \brief Storage of components in our registry.
+///
+/// This class stores components together in a `Column` which are indexed by
+/// their `ComponentId`.
 class Components {
 public:
   // TODO (bgluzman): docstring
   template <Component T, typename... Ts>
-  void emplace(EntityId entityId, Ts &&...args);
+  void emplace(EntityId entityId, Ts&&...args);
 
   // TODO (bgluzman): docstring
-  void set(EntityId entity, ComponentId component, std::any value);
+  template <Component T>
+  void set(EntityId entity, const T& value);
 
   // TODO (bgluzman): docstring
-  bool remove(EntityId entity, ComponentId component);
+  template <Component T>
+  bool remove(EntityId entity);
 
   // TODO (bgluzman): docstring
-  bool has(EntityId entityId, ComponentId component) const noexcept;
+  template <std::ranges::range T>
+  bool removeAll(EntityId entity, const T& componentsToRemove);
 
   // TODO (bgluzman): docstring
-  std::any *get(EntityId entityId, ComponentId component);
+  template <typename T>
+  bool has(EntityId entityId) const noexcept;
 
   // TODO (bgluzman): docstring
-  const std::any *get(EntityId entityId, ComponentId component) const;
+  template <Component T>
+  T *get(EntityId entityId);
+
+  // TODO (bgluzman): docstring
+  template <Component T>
+  const T *get(EntityId entityId) const;
 
 private:
   std::map<ComponentId, Column> components_;
 };
 
-} // namespace bad::internal
+}  // namespace bad::internal
 
 #endif
