689aea5f4644c5c75bb99e8ebc7416fef93a3291
Brian Gluzman <bgluzman@gmail.com>
2024-04-29 12:46:27 -0700

    rearrange test file in terms of dependency

--- test/badecs_test.cpp
+++ test/badecs_test.cpp
@@ -19,6 +19,90 @@ std::ostream& operator<<(std::ostream& os, const Position& position) {
 namespace bad {
 namespace internal {
 
+TEST(EntitiesTest, ReserveInstantiate) {
+  Entities entities;
+
+  // Reserve two entities before instantiating them.
+  EntityId id1 = entities.reserve();
+  EntityId id2 = entities.reserve();
+  // Ensure reservations are unique.
+  EXPECT_NE(id1, id2);
+  EXPECT_FALSE(entities.has(id1));
+  EXPECT_FALSE(entities.has(id2));
+
+  // Instantiate the first entity.
+  entities.instantiate(id1);
+  EXPECT_TRUE(entities.has(id1));
+  EXPECT_FALSE(entities.has(id2));
+
+  // Instantiate the second entity.
+  entities.instantiate(id2);
+  EXPECT_TRUE(entities.has(id1));
+  EXPECT_TRUE(entities.has(id2));
+
+  // Now instantiate a third entity and ensure it's unique w.r.t. the first two.
+  EntityId id3 = entities.reserve();
+  entities.instantiate(id3);
+  EXPECT_NE(id1, id3);
+  EXPECT_NE(id2, id3);
+  EXPECT_TRUE(entities.has(id3));
+}
+
+TEST(EntitiesTest, EntityComponents) {
+  Entities entities;
+
+  // Ensure non-existent entity has no components.
+  EXPECT_FALSE(entities.hasComponent(0, componentId<Position>));
+  // Ensure adding a component to a non-existent entity fails.
+  EXPECT_FALSE(entities.addComponent(0, componentId<Position>));
+
+  // Reserve and instantiate an entity.
+  EntityId id = entities.reserve();
+  entities.instantiate(id);
+  EXPECT_TRUE(entities.has(id));
+  EXPECT_FALSE(entities.hasComponent(id, componentId<Position>));
+
+  // Add a component to the entity and check for its existence.
+  entities.addComponent(id, componentId<Position>);
+  EXPECT_TRUE(entities.hasComponent(id, componentId<Position>));
+
+  // Remove the component and check for its absence.
+  entities.removeComponent(id, componentId<Position>);
+  EXPECT_FALSE(entities.hasComponent(id, componentId<Position>));
+}
+
+TEST(EntitiesTest, Remove) {
+  Entities                                       entities;
+  std::optional<std::unordered_set<ComponentId>> components = std::nullopt;
+
+  // Try removing an entity that doesn't even exist.
+  ASSERT_FALSE(entities.has(0));
+  components = entities.remove(0);
+  EXPECT_FALSE(components.has_value());
+
+  // Reserve and instantiate an entity.
+  EntityId id = entities.reserve();
+  entities.instantiate(id);
+  ASSERT_TRUE(entities.has(id));
+
+  // Remove the entity before adding any components.
+  components = entities.remove(id);
+  EXPECT_TRUE(components.has_value());  // should be empty, but existent
+  EXPECT_FALSE(entities.has(id));
+
+  // Remove an entity with components.
+  id = entities.reserve();
+  entities.instantiate(id);
+  ASSERT_TRUE(entities.has(id));
+  ASSERT_TRUE(entities.addComponent(id, componentId<Position>));
+  ASSERT_TRUE(entities.hasComponent(id, componentId<Position>));
+  components = entities.remove(id);
+  ASSERT_TRUE(components.has_value());
+  ASSERT_EQ(components->size(), 1);
+  EXPECT_EQ(*components->begin(), componentId<Position>);
+  EXPECT_FALSE(entities.has(id));
+}
+
 template <typename T>
 testing::AssertionResult TestColumnValue(const Column& column,
                                          EntityId entityId, T reference) {
@@ -168,6 +252,115 @@ TEST(ColumnTest, Iterators) {
   }
 }
 
+class ViewImplTest : public testing::Test {
+protected:
+  void SetUp() override {
+    posColumn.emplace<Position>(0, 1, 2);
+    posColumn.emplace<Position>(1, 3, 4);
+    posColumn.emplace<Position>(2, 5, 6);
+    intColumn.set(1, 42);
+    boolColumn.set(0, true);
+    boolColumn.set(2, false);
+    floatColumn.set(2, 123.f);
+  }
+
+  Column posColumn, intColumn, boolColumn, floatColumn;
+};
+
+TEST_F(ViewImplTest, EmptyView) {
+  ViewImpl<Position> emptyView;
+  EXPECT_EQ(emptyView.begin(), emptyView.end());
+
+  ViewImpl<Position> nullView1({nullptr});
+  EXPECT_EQ(nullView1.begin(), nullView1.end());
+  ViewImpl<Position, int> nullView2({&posColumn, nullptr});
+  EXPECT_EQ(nullView2.begin(), nullView2.end());
+
+  Column          emptyColumn;
+  ViewImpl<float> emptyColumnView1({&emptyColumn});
+  EXPECT_EQ(emptyColumnView1.begin(), emptyColumnView1.end());
+  ViewImpl<Position, float> emptyColumnView2({&posColumn, &emptyColumn});
+  EXPECT_EQ(emptyColumnView2.begin(), emptyColumnView2.end());
+}
+
+TEST_F(ViewImplTest, SingleComponent) {
+  ViewImpl<Position> view({&posColumn});
+  auto               begin = view.begin();
+  auto               end = view.end();
+
+  EXPECT_EQ(std::distance(begin, end), posColumn.size());
+
+  for (auto [columnKV, viewTuple] : std::views::zip(posColumn, view)) {
+    auto [columnEntityId, posAny] = columnKV;
+    auto [viewEntityId, viewPos] = viewTuple;
+    SCOPED_TRACE("columnEntityId=" + std::to_string(columnEntityId) +
+                 ", viewEntityId=" + std::to_string(viewEntityId));
+    ASSERT_TRUE(posAny.has_value());
+    ASSERT_EQ(posAny.type(), typeid(Position));
+    EXPECT_EQ(columnEntityId, viewEntityId);
+    EXPECT_EQ(std::any_cast<Position>(posAny), viewPos);
+  }
+}
+
+TEST_F(ViewImplTest, MultiComponent) {
+  ViewImpl<Position, bool> view({&posColumn, &boolColumn});
+
+  // Construct a map of the relationships from the view since iteration order
+  // is not guaranteed.
+  std::map<EntityId, std::tuple<Position, bool>> viewData;
+  std::transform(view.begin(), view.end(),
+                 std::inserter(viewData, viewData.end()),
+                 [](const auto& tuple) {
+                   auto [entityId, pos, b] = tuple;
+                   return std::make_pair(entityId, std::make_tuple(pos, b));
+                 });
+
+  EXPECT_EQ(viewData.size(), 2);
+
+  ASSERT_TRUE(viewData.contains(0));
+  EXPECT_EQ(std::get<0>(viewData[0]), (Position{1, 2}));
+  EXPECT_EQ(std::get<1>(viewData[0]), true);
+
+  ASSERT_TRUE(viewData.contains(2));
+  EXPECT_EQ(std::get<0>(viewData[2]), (Position{5, 6}));
+  EXPECT_EQ(std::get<1>(viewData[2]), false);
+}
+
+TEST_F(ViewImplTest, EmptyIntersection) {
+  // `intColumn` and `boolColumn` have disjoint sets of entities.
+  ViewImpl<Position, int, bool> view({&posColumn, &intColumn, &boolColumn});
+  EXPECT_EQ(view.begin(), view.end());
+}
+
+TEST_F(ViewImplTest, FilterViewedColumn) {
+  ViewImpl<Position, bool> view({&posColumn, &boolColumn});
+  // Filtering on any column which is viewed results in no values iterated.
+  view.filterColumn(&boolColumn);
+  EXPECT_EQ(view.begin(), view.end());
+}
+
+TEST_F(ViewImplTest, FilterDisjointColumn) {
+  ViewImpl<Position, bool> view({&posColumn, &boolColumn});
+  // Filtering on a column which is disjoint w.r.t. the viewed columns should
+  // not impact the values iterated.
+  view.filterColumn(&intColumn);
+  EXPECT_EQ(std::distance(view.begin(), view.end()), 2);
+}
+
+TEST_F(ViewImplTest, FilterOverlappingColumn) {
+  ViewImpl<Position, bool> view({&posColumn, &boolColumn});
+  // Filtering on a column which overlaps with the viewed columns should
+  // cause those entities to not appear when being iterated. Here, that is
+  // entity with id=2.
+  view.filterColumn(&floatColumn);
+  EXPECT_EQ(std::distance(view.begin(), view.end()), 1);
+  // Only entity with id=0 should remain.
+  auto [entityId, pos, b] = *view.begin();
+  EXPECT_EQ(entityId, 0);
+  EXPECT_EQ(pos, (Position{1, 2}));
+  EXPECT_EQ(b, true);
+}
+
 template <typename T>
 testing::AssertionResult TestComponentValue(const Components& components,
                                             EntityId entityId, T reference) {
@@ -184,13 +377,13 @@ testing::AssertionResult TestComponentValue(const Components& components,
   return testing::AssertionSuccess();
 }
 
-TEST(Components, EmptyInvariants) {
+TEST(ComponentsTest, EmptyInvariants) {
   Components components;
   EXPECT_EQ(components.has<Position>(0), false);
   EXPECT_EQ(components.get<Position>(0), nullptr);
 }
 
-TEST(Components, Emplace) {
+TEST(ComponentsTest, Emplace) {
   Components components;
 
   ASSERT_EQ(components.has<Position>(0), false);
@@ -300,199 +493,6 @@ TEST(ComponentsTest, RemoveAll) {
   EXPECT_EQ(components.get<Position>(2), nullptr);
   EXPECT_EQ(components.has<Position>(10), false);
   EXPECT_EQ(components.get<Position>(10), nullptr);
-}
-
-TEST(EntitiesTest, ReserveInstantiate) {
-  Entities entities;
-
-  // Reserve two entities before instantiating them.
-  EntityId id1 = entities.reserve();
-  EntityId id2 = entities.reserve();
-  // Ensure reservations are unique.
-  EXPECT_NE(id1, id2);
-  EXPECT_FALSE(entities.has(id1));
-  EXPECT_FALSE(entities.has(id2));
-
-  // Instantiate the first entity.
-  entities.instantiate(id1);
-  EXPECT_TRUE(entities.has(id1));
-  EXPECT_FALSE(entities.has(id2));
-
-  // Instantiate the second entity.
-  entities.instantiate(id2);
-  EXPECT_TRUE(entities.has(id1));
-  EXPECT_TRUE(entities.has(id2));
-
-  // Now instantiate a third entity and ensure it's unique w.r.t. the first two.
-  EntityId id3 = entities.reserve();
-  entities.instantiate(id3);
-  EXPECT_NE(id1, id3);
-  EXPECT_NE(id2, id3);
-  EXPECT_TRUE(entities.has(id3));
-}
-
-TEST(EntitiesTest, EntityComponents) {
-  Entities entities;
-
-  // Ensure non-existent entity has no components.
-  EXPECT_FALSE(entities.hasComponent(0, componentId<Position>));
-  // Ensure adding a component to a non-existent entity fails.
-  EXPECT_FALSE(entities.addComponent(0, componentId<Position>));
-
-  // Reserve and instantiate an entity.
-  EntityId id = entities.reserve();
-  entities.instantiate(id);
-  EXPECT_TRUE(entities.has(id));
-  EXPECT_FALSE(entities.hasComponent(id, componentId<Position>));
-
-  // Add a component to the entity and check for its existence.
-  entities.addComponent(id, componentId<Position>);
-  EXPECT_TRUE(entities.hasComponent(id, componentId<Position>));
-
-  // Remove the component and check for its absence.
-  entities.removeComponent(id, componentId<Position>);
-  EXPECT_FALSE(entities.hasComponent(id, componentId<Position>));
-}
-
-TEST(EntitiesTest, Remove) {
-  Entities                                       entities;
-  std::optional<std::unordered_set<ComponentId>> components = std::nullopt;
-
-  // Try removing an entity that doesn't even exist.
-  ASSERT_FALSE(entities.has(0));
-  components = entities.remove(0);
-  EXPECT_FALSE(components.has_value());
-
-  // Reserve and instantiate an entity.
-  EntityId id = entities.reserve();
-  entities.instantiate(id);
-  ASSERT_TRUE(entities.has(id));
-
-  // Remove the entity before adding any components.
-  components = entities.remove(id);
-  EXPECT_TRUE(components.has_value());  // should be empty, but existent
-  EXPECT_FALSE(entities.has(id));
-
-  // Remove an entity with components.
-  id = entities.reserve();
-  entities.instantiate(id);
-  ASSERT_TRUE(entities.has(id));
-  ASSERT_TRUE(entities.addComponent(id, componentId<Position>));
-  ASSERT_TRUE(entities.hasComponent(id, componentId<Position>));
-  components = entities.remove(id);
-  ASSERT_TRUE(components.has_value());
-  ASSERT_EQ(components->size(), 1);
-  EXPECT_EQ(*components->begin(), componentId<Position>);
-  EXPECT_FALSE(entities.has(id));
-}
-
-class ViewImplTest : public testing::Test {
-protected:
-  void SetUp() override {
-    posColumn.emplace<Position>(0, 1, 2);
-    posColumn.emplace<Position>(1, 3, 4);
-    posColumn.emplace<Position>(2, 5, 6);
-    intColumn.set(1, 42);
-    boolColumn.set(0, true);
-    boolColumn.set(2, false);
-    floatColumn.set(2, 123.f);
-  }
-
-  Column posColumn, intColumn, boolColumn, floatColumn;
-};
-
-TEST_F(ViewImplTest, EmptyView) {
-  ViewImpl<Position> emptyView;
-  EXPECT_EQ(emptyView.begin(), emptyView.end());
-
-  ViewImpl<Position> nullView1({nullptr});
-  EXPECT_EQ(nullView1.begin(), nullView1.end());
-  ViewImpl<Position, int> nullView2({&posColumn, nullptr});
-  EXPECT_EQ(nullView2.begin(), nullView2.end());
-
-  Column          emptyColumn;
-  ViewImpl<float> emptyColumnView1({&emptyColumn});
-  EXPECT_EQ(emptyColumnView1.begin(), emptyColumnView1.end());
-  ViewImpl<Position, float> emptyColumnView2({&posColumn, &emptyColumn});
-  EXPECT_EQ(emptyColumnView2.begin(), emptyColumnView2.end());
-}
-
-TEST_F(ViewImplTest, SingleComponent) {
-  ViewImpl<Position> view({&posColumn});
-  auto               begin = view.begin();
-  auto               end = view.end();
-
-  EXPECT_EQ(std::distance(begin, end), posColumn.size());
-
-  for (auto [columnKV, viewTuple] : std::views::zip(posColumn, view)) {
-    auto [columnEntityId, posAny] = columnKV;
-    auto [viewEntityId, viewPos] = viewTuple;
-    SCOPED_TRACE("columnEntityId=" + std::to_string(columnEntityId) +
-                 ", viewEntityId=" + std::to_string(viewEntityId));
-    ASSERT_TRUE(posAny.has_value());
-    ASSERT_EQ(posAny.type(), typeid(Position));
-    EXPECT_EQ(columnEntityId, viewEntityId);
-    EXPECT_EQ(std::any_cast<Position>(posAny), viewPos);
-  }
-}
-
-TEST_F(ViewImplTest, MultiComponent) {
-  ViewImpl<Position, bool> view({&posColumn, &boolColumn});
-
-  // Construct a map of the relationships from the view since iteration order
-  // is not guaranteed.
-  std::map<EntityId, std::tuple<Position, bool>> viewData;
-  std::transform(view.begin(), view.end(),
-                 std::inserter(viewData, viewData.end()),
-                 [](const auto& tuple) {
-                   auto [entityId, pos, b] = tuple;
-                   return std::make_pair(entityId, std::make_tuple(pos, b));
-                 });
-
-  EXPECT_EQ(viewData.size(), 2);
-
-  ASSERT_TRUE(viewData.contains(0));
-  EXPECT_EQ(std::get<0>(viewData[0]), (Position{1, 2}));
-  EXPECT_EQ(std::get<1>(viewData[0]), true);
-
-  ASSERT_TRUE(viewData.contains(2));
-  EXPECT_EQ(std::get<0>(viewData[2]), (Position{5, 6}));
-  EXPECT_EQ(std::get<1>(viewData[2]), false);
-}
-
-TEST_F(ViewImplTest, EmptyIntersection) {
-  // `intColumn` and `boolColumn` have disjoint sets of entities.
-  ViewImpl<Position, int, bool> view({&posColumn, &intColumn, &boolColumn});
-  EXPECT_EQ(view.begin(), view.end());
-}
-
-TEST_F(ViewImplTest, FilterViewedColumn) {
-  ViewImpl<Position, bool> view({&posColumn, &boolColumn});
-  // Filtering on any column which is viewed results in no values iterated.
-  view.filterColumn(&boolColumn);
-  EXPECT_EQ(view.begin(), view.end());
-}
-
-TEST_F(ViewImplTest, FilterDisjointColumn) {
-  ViewImpl<Position, bool> view({&posColumn, &boolColumn});
-  // Filtering on a column which is disjoint w.r.t. the viewed columns should
-  // not impact the values iterated.
-  view.filterColumn(&intColumn);
-  EXPECT_EQ(std::distance(view.begin(), view.end()), 2);
-}
-
-TEST_F(ViewImplTest, FilterOverlappingColumn) {
-  ViewImpl<Position, bool> view({&posColumn, &boolColumn});
-  // Filtering on a column which overlaps with the viewed columns should
-  // cause those entities to not appear when being iterated. Here, that is
-  // entity with id=2.
-  view.filterColumn(&floatColumn);
-  EXPECT_EQ(std::distance(view.begin(), view.end()), 1);
-  // Only entity with id=0 should remain.
-  auto [entityId, pos, b] = *view.begin();
-  EXPECT_EQ(entityId, 0);
-  EXPECT_EQ(pos, (Position{1, 2}));
-  EXPECT_EQ(b, true);
 }
 
 }  // namespace internal
