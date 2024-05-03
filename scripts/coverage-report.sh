80ceb5aa9cefdce7d0320b23ceb97dbf9ac899d0
Brian Gluzman <bgluzman@gmail.com>
2024-05-03 10:37:08 -0700

    pass gcov-executable to gcovr

--- scripts/coverage-report.sh
+++ scripts/coverage-report.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$(dirname "$0")"
 cd "$SCRIPT_DIR/.." || exit 1
 mkdir -p coverage
-gcovr --filter include/badecs --html-details coverage/coverage.html
+gcovr "$@" --filter include/badecs --html-details coverage/coverage.html
